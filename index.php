025802087a51365048cd983c8cb15e7913d3c1a0
JuniorLima22 <negstonseventos@hotmail.com>
2019-07-25 22:38:53 -0300

    criamos a classe page

--- index.php
+++ index.php
@@ -2,17 +2,23 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use \Hcode\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
+   
+  //Chama o metodo __construct e carrega o header
+	$page = new Page();
 
-	$results = $sql->select("SELECT * FROM tb_users");
+	//Adiciona o arquivo que tem o H1 hello (index.html)
+	$page->setTpl("index");
 
-	echo json_encode($results);
+	//Aqui acaba a execução, e o PHP limpa a memoria e chama o método __destruction e carrega o footer
+
 
 });
 
