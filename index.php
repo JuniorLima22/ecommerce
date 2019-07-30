32bdfe4ae32c74f07f3ad1ca47cb1bb9cc50641a
JuniorLima22 <negstonseventos@hotmail.com>
2019-07-30 07:01:38 -0300

    criando page admin e tpl admin

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -18,8 +19,16 @@ $app->get('/', function() {
 	$page->setTpl("index");
 
 	//Aqui acaba a execução, e o PHP limpa a memoria e chama o método __destruction e carrega o footer
+});
 
+//Rota para page admin
+$app->get('/admin', function() {
+   
+	$page = new PageAdmin();
 
+	$page->setTpl("index");
+
+	//Aqui acaba a execução, e o PHP limpa a memoria e chama o método __destruction e carrega o footer
 });
 
 $app->run();
---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,13 @@
+<?php
+namespace Hcode;
+
+class PageAdmin extends Page {
+	public function __construct($opts = array(), $tpl_dir = "/views/admin/"){
+
+		//Repete o método construct da class Page
+		parent::__construct($opts, $tpl_dir);
+
+	}
+
+}
+?>
